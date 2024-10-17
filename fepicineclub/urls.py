4e322f18a5c20eb59d2560064492fad49d07ebf0
Ana Helouise <anahelouise.ss@gmail.com>
2024-10-17 16:54:59 -0300

    catalogo de filmes

--- fepicineclub/urls.py
+++ fepicineclub/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from fepicineclub.views import home, root_view, my_view, user_view
+
+from fepicineclub.views import root_view, home, filmes, forum, contato
 
 urlpatterns = [
-    path('home/', home),
+    path('home/', home, name='home'),
     path('', root_view),
-    path('sobre/', my_view),
-    path('user/<str:username>/', user_view),
-]
+    path('filmes/', filmes, name='filmes'),
+    path('forum/', forum, name='forum'),
+    path('contato/', contato, name='contato'),
+]  
--- fepicineclub/views.py
+++ fepicineclub/views.py
@@ -3,13 +3,16 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
-
-def my_view(request):
-    return HttpResponse("Uma teste string de resposta")
-
-def user_view(request, username):
-    return HttpResponse(f"Perfil do usuário: {username}")
+    return render(request, "home.html") #pagina home do projeto
 
 def root_view(request):
-    return HttpResponse("Estamos na raiz. Porta 8000")
+    return render(request, "home.html") #pagina inicial do runserver
+
+def filmes(request):
+    return render(request, "catalogofilmes.html") #Pagina sobre a desenvolvedora
+
+def forum(request):
+    return render(request, "tropas.html") #Pagina sobre as tropas
+
+def contato(request):
+    return render(request, "contato.html")
