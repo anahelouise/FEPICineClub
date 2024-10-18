3ef83bc7f28d5c314b901ade309b6291134243b8
Ana Helouise <anahelouise.ss@gmail.com>
2024-10-17 21:43:36 -0300

    mudanças

--- fepicineclub/views.py
+++ fepicineclub/views.py
@@ -9,7 +9,7 @@ def root_view(request):
     return render(request, "home.html") #pagina inicial do runserver
 
 def filmes(request):
-    return render(request, "catalogofilmes.html") #Pagina sobre a desenvolvedora
+    return render(request, "catalogo.html") #Pagina sobre a desenvolvedora
 
 def forum(request):
     return render(request, "tropas.html") #Pagina sobre as tropas
